40f3a28a2ed99588ca263c4acf28cb027b7da3bf
rajibkhan976 <rajibkhan976@gmail.com>
2024-04-03 11:00:22 +0800

    db tables created

---
+++ movierating/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=24)
+    password = models.CharField(max_length=8)
+    email = models.EmailField(max_length=50,unique=True)
+
+class Movie(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150)
+    genre = models.CharField(max_length=150, null=True)
+    rating = models.CharField(max_length=150, null=True)
+    release_date = models.DateField()
+
+class Rating(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
+    movie_id = models.ManyToManyField("Movie", related_name="+")
+    rating = models.FloatField()
